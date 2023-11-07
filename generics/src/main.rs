8e8d2bf70454a44614d0238f11bdd1c11b36f129
Mat Pataki <matpataki@gmail.com>
2023-11-06 23:14:15 -0500

    play around with generics and result

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,21 +1,21 @@
 fn main() {
-    //let numbers = vec![1, 2, 3, 100, 4, 5, 6, 7, 8];
-    let numbers: Vec<i32> = vec![];
+    let numbers1 = vec![1, 2, 3, 100, 4, 5, 6, 7, 8];
+    let result1 = largest(&numbers1);
+    print_result(result1);
 
-    match largest(&numbers) {
-        Ok(n) => println!("The largest number is {}", n),
-        Err(e) => println!("{}", e),
-    }
+    let numbers2: Vec<i32> = vec![];
+    let result2 = largest(&numbers2);
+    print_result(result2);
 }
 
-fn largest<T: std::cmp::PartialOrd>(numbers: &[T]) -> Result<&T, &'static str> {
-    if numbers.len() == 0 {
+fn largest<T: std::cmp::PartialOrd>(stuff: &[T]) -> Result<&T, &'static str> {
+    if stuff.len() == 0 {
         return Result::Err("There are no items in the list");
     }
 
-    let mut largest = &numbers[0];
+    let mut largest = &stuff[0];
 
-    for n in numbers {
+    for n in stuff {
         if n > largest {
             largest = n;
         }
@@ -23,4 +23,12 @@ fn largest<T: std::cmp::PartialOrd>(numbers: &[T]) -> Result<&T, &'static str> {
 
     return Result::Ok(largest);
 }
+
+fn print_result<T: std::fmt::Debug>(result: Result<T, &'static str>) {
+    match result {
+        Ok(n) => println!("The largest number is {n:?}"),
+        Err(e) => println!("{e:?}"),
+    }
+}
+
 
